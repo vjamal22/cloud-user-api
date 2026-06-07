e196dc13ca669f48f98c43417ffbe05b986ffacb
vjamal22 <valdmodel@gmail.com>
2026-06-07 18:06:45 +0100

    Phase 9: complete media upload and analysis interface

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,15 @@
 resource "aws_s3_bucket" "media" {
   bucket = "fitness-app-media-jamalv-2026"
 }
+
+resource "aws_s3_bucket_cors_configuration" "media_cors" {
+  bucket = aws_s3_bucket.media.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET"]
+    allowed_origins = ["http://localhost:5173"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
